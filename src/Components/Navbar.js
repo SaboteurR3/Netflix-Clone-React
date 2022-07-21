8e9f62f7a02db676a0f15a81b8d1ae47c342ab77
SaboteuR3 <bidzishvili145@gmail.com>
2022-07-21 14:26:58 +0400

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-/*rafce*/
+
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
